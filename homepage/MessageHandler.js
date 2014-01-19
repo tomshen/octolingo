82e65e8cf2e7b7671d0c20c788b879193ca1966f
Taylor Poulos <poulos.taylor.w@gmail.com>
2014-01-19 08:59:51 -0500

    Added homepage

---
+++ homepage/MessageHandler.js
@@ -0,0 +1,37 @@
+C = {};
+
+C.LANGUAGES = [{
+		name: "English",
+		transcribe: "en",
+		translate: "en"
+	},{
+		name: "French",
+		transcribe: "fr",
+		translate: "fr"
+	},{
+		name: "Spanish",
+		transcribe: "sp",
+		translate: "sp"
+	},{
+		name: "Italian",
+		transcribe: "it",
+		translate: "it"
+	},{
+		name: "German",
+		transcribe: "gr",
+		translate: "gr"
+	},{
+		name: "Japanese",
+		transcribe: "jp",
+		translate: "jp"
+	},{
+		name: "Korean",
+		transcribe: "ko",
+		translate: "ko"
+	},{
+		name: "Chinese",
+		transcribe: "cn",
+		translate: "cn"
+	}]
+
+
